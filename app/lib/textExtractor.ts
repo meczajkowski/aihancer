880b2c8b8b96605b96543d6274b313730f404894
meczajkowski <michal.erik.czajkowski@gmail.com>
2024-10-23 23:28:18 +0200

    feat: next.js init

--- app/lib/textExtractor.ts
+++
@@ -1,9 +0,0 @@
-import officeparser from 'officeparser'
-
-export const extractTextFromPDF = async (pdfBuffer: Buffer) => {
-  const extractedText = await officeparser.parseOfficeAsync(pdfBuffer)
-  if (!extractedText) {
-    throw new Error('Failed to extract text from the PDF.')
-  }
-  return extractedText
-}
