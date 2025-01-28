0a3d43af8f7c51a6210d9869ad18fb9c5711accc
meczajkowski <michal.erik.czajkowski@gmail.com>
2025-01-29 00:07:48 +0100

    chore: adjust validation schema to work on both server/client

--- schemas/stepOneSchema.ts
+++ schemas/stepOneSchema.ts
@@ -1,36 +1,18 @@
 import { z } from 'zod';
 
-const allowedFileTypes = [
+const ACCEPTED_FILE_TYPES = [
   'application/pdf',
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
 ];
 const MAX_FILE_SIZE = 2000000;
 
-// Helper function to check if we're in a browser environment
-const isBrowser = typeof window !== 'undefined';
-
 export const stepOneSchema = z.object({
   cvFile: z
     .any()
-    .refine((file) => {
-      if (!isBrowser) return true; // Skip validation on server
-      return file instanceof File;
-    }, 'Must be a file')
-    .refine((file) => {
-      if (!isBrowser) return true; // Skip validation on server
-      return file.name;
-    }, 'File is required')
-    .refine((file) => {
-      if (!isBrowser) return true; // Skip validation on server
-      return file.size < MAX_FILE_SIZE;
-    }, 'Max size is 2MB.')
+    .refine((file) => file?.name, 'File is required.')
+    .refine((file) => file?.size < MAX_FILE_SIZE, 'Max size is 2MB.')
     .refine(
-      (file) => {
-        if (!isBrowser) return true; // Skip validation on server
-        return allowedFileTypes.includes(file.type);
-      },
-      {
-        message: 'Invalid file type. Only PDF and DOCX files are allowed.',
-      },
+      (file) => ACCEPTED_FILE_TYPES.includes(file?.type),
+      'Invalid file type. Only PDF and DOCX files are allowed.',
     ),
 });
